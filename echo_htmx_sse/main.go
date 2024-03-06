6da5ae7bbf92f6be5d23b0209deffd4ff5d67c17
Eduard Khiaev <eddietheone1984@gmail.com>
2024-03-06 09:00:00 +0000

    echo_htmx_sse

---
+++ echo_htmx_sse/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	// Serve the index.html file for the root path
+	e.GET("/", func(c echo.Context) error {
+		return c.File("index.html")
+	})
+
+	// Handle the SSE endpoint
+	e.GET("/events", func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderContentType, "text/event-stream")
+		c.Response().Header().Set(echo.HeaderCacheControl, "no-cache")
+		c.Response().Header().Set(echo.HeaderConnection, "keep-alive")
+		c.Response().WriteHeader(http.StatusOK)
+
+		randSource := rand.NewSource(time.Now().UnixNano())
+		randGen := rand.New(randSource)
+
+		for {
+			randNum := randGen.Intn(100)
+			event := fmt.Sprintf("data: Random number: %d\n\n", randNum)
+			_, err := c.Response().Write([]byte(event))
+			if err != nil {
+				return err
+			}
+			c.Response().Flush()
+			time.Sleep(3 * time.Second)
+		}
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
